7524cfc4577c394d4c97a9ccf18ee289c8a49ff8
Christopher Bryant <cbryant@berkeley.edu>
2017-09-21 20:24:53 -0700

    Add files via upload

--- totalityNASA2JSON.py
+++ totalityNASA2JSON.py
@@ -1,12 +1,19 @@
+import re
 import json
+import numpy as np
 import pandas as pd
+from datetime import datetime, timedelta
 
-def totality2JSON(totalityfile, outputGeoJSON):
+def totalityNASA2JSON(totalityfile, outputGeoJSON):
     '''
     USAGE: 
     Converts raw text file of NASA eclipse path data into formatted GeoJSON file 
     containing both the totality path center-line and the polygon enclosing the 
-    region of totality.
+    region of totality. Also creates a dataframe of the same information.
+    
+    ARGUMENTS:
+    totalityfile - text file containing raw data from NASA
+    outputGeoJSON - output location for generated GeoJSON
     
     RETURNS:
     df - dataframe of eclipse path data extracted from "totalityfile"
@@ -71,15 +78,80 @@ def totality2JSON(totalityfile, outputGeoJSON):
 
     with open(outputGeoJSON, 'w') as f:
         json.dump(geoJSON, f)
-        
+    
+    print('Eclipse path GeoJSON created and saved to "%s"' % outputGeoJSON)
+    
     return df
 
-##### ------------------------------------- MAIN ------------------------------------- ##### 
+def centerEclipseJSON(df, outputCenterJSON):
+    '''
+    USAGE:
+    Interpolate NASA coordinates to get centerline coordinates every 30s.
+    Outputs a new GeoJSON with just points along the totality centerline.
+    
+    ARGUMENTS:
+    df - dataframe created from totalityNASA2JSON
+    outputCenterJSON - output location for generated GeoJSON
+    '''    
+    
+    df['CentralLonLat']
+    coordlist = list(df['CentralLonLat'])
+    interp = []
+    
+    print('Interpolating coordinates...')
+    for idx, val in enumerate(coordlist):
+        A = np.array(val)
+        interp.append(list(A))
+        if idx < len(df) - 1:
+            B = np.array(coordlist[idx + 1])
+            diff = B - A
+            for i in [1, 2, 3]:
+                v = A + i/4*diff
+                interp.append(list(v))
 
+    t0 = datetime(2017, 8, 21, 16, 50, 0) # first time in data collection
+    timelist = []
+    for i in range(len(interp)):
+        t = timedelta(seconds = i * 30) + t0
+        timelist.append(str(t.time()))
+
+    timeseries = pd.Series(timelist)
+    interpseries = pd.Series(interp)
+    inter_df = pd.DataFrame(timeseries, columns = ['Time'])
+    inter_df['CentralLonLat'] = interpseries
+    inter_df
+
+    features = []
+    for i in range(len(inter_df)):
+        point = {"type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": inter_df.iloc[i]['CentralLonLat']
+            },
+            "properties": {
+                "Time" : inter_df.iloc[i]['Time'] 
+            }
+        }
+        features.append(point)
+
+    centerJSON = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    with open(outputCenterJSON, 'w') as f:
+        json.dump(centerJSON, f)
+        
+    print('Coordinates interpolated and saved to "%s".' % outputCenterJSON)
+
+##### ------------------------------------- MAIN ------------------------------------- #####
+    
 def main():
     totalityfile = 'Resources/2017_08_21_NASAEclipsePath.txt'
     outputGeoJSON = 'Resources/eclipseGeoJSON.json'
-    df = totality2JSON(totalityfile, outputGeoJSON)
+    outputCenterJSON = 'Resources/eclipseCenterJSON.json'
+    df = totalityNASA2JSON(totalityfile, outputGeoJSON)
+    centerEclipseJSON(df, outputCenterJSON)
     
 if __name__ == '__main__':
     main() 
